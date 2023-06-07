ec0eaeea7d0992d5315bb091e539e7fbdaed42bf
henry.rojas <henry.rojas@globant.com>
2023-06-07 18:40:26 -0500

    otra session

--- src/App.js
+++ src/App.js
@@ -17,7 +17,15 @@ function App() {
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => todo.completed).length;
-  const totalTodos = 0;
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    }
+  );
 
   console.log('Los usuarios buscan TODOS de ' + searchValue);
 
@@ -27,7 +35,7 @@ function App() {
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue}/>
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
         ))}
       </TodoList>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,16 @@
 import './TodoItem.css';
 
-function TodoItem(props) {
+function TodoItem({
+  completed,
+  text
+}) {
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}>
+      <span className={`Icon Icon-check ${completed && "Icon-check--active"}`}>
         V
       </span>
-      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
-        {props.text}
+      <p className={`TodoItem-p ${completed && "TodoItem-p--complete"}`}>
+        {text}
       </p>
       <span className="Icon Icon-delete">
         X
